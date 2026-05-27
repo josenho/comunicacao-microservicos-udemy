6c110d6fde03a6d6e6957801bf8849763315d9c2
Jose Mateus <112735456+josenho@users.noreply.github.com>
2026-05-27 11:14:50 -0300

    conteudo das aulas 31 e 32

---
+++ product-api/src/main/java/br/com/curso_udemy/product_api/modules/product/model/Category.java
@@ -0,0 +1,21 @@
+package br.com.curso_udemy.product_api.modules.product.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "CATEGORY")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+}
