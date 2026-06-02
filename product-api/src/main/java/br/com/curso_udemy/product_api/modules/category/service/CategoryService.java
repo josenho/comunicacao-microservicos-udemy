fbcb381c9b8da0318f1fa66a24b0d81c7144c662
Jose Mateus <112735456+josenho@users.noreply.github.com>
2026-06-02 12:04:15 -0300

    conteúdo da aula 36. Agora é possivel realizar uma requisição GET em /api/category e ver as categorias

--- product-api/src/main/java/br/com/curso_udemy/product_api/modules/category/service/CategoryService.java
+++ product-api/src/main/java/br/com/curso_udemy/product_api/modules/category/service/CategoryService.java
@@ -8,6 +8,9 @@ import br.com.curso_udemy.product_api.modules.category.repository.CategoryReposi
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
 @Service
@@ -16,7 +19,33 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    public CategoryResponse findByIdResponse(Integer id) {
+        return CategoryResponse.of(findById(id));
+    }
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository
+                .findAll()
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<CategoryResponse> findByDescription(String description) {
+        if (isEmpty(description)){
+            throw new ValidationException("The category description must be informed.");
+        }
+        return categoryRepository
+                .findByDescriptionIgnoreCaseContaining(description)
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
+    }
+
     public Category findById(Integer id){
+        if (isEmpty(id)){
+            throw new ValidationException("The category ID was not informed.");
+        }
         return categoryRepository
                 .findById(id)
                 .orElseThrow(() -> new ValidationException("There's no category for the given ID."));
